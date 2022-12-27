a2ab2d7d05a054b0319558b67b573556c8b0fc0c
Zahir Azmi <zahir.arief.azmi@gmail.com>
2022-12-27 13:41:51 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/ferry_booking/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ferry_booking
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/sqldata.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/foundation.dart';
+import 'package:sqflite/sqflite.dart' as sql;
+
+class SQLData {
+  static get createAt => null;
+
+
+  
+}
